54f4fb486e50ea206af66da08b9686ef8d70918c
hello@cyan.red <hello@cyan.red>
2018-04-10 12:58:32 -0700

    Added forms: review, delete, rename

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,37 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, SelectField, DecimalField, IntegerField
 from wtforms.validators import DataRequired, ValidationError, Length, Email
+from app.main.models import Topic, Review
+from app import create_app
 
 
-# class ExampleForm(FlaskForm):
-#     post = TextAreaField('Say something', validators=[DataRequired(),
-#         Length(min=1, max=280)])
-#     submit = SubmitField('Post')
+
+def choices():
+    app = create_app()
+    with app.app_context():
+        topics = Topic.query.order_by(Topic.filename).all()
+        choices = [(t.filename, t.filename) for t in topics]
+        return choices
+
+
+class ReviewForm(FlaskForm):
+    filename = SelectField('Topic:', choices=choices())
+    time_spent = IntegerField('Time spent:', validators=[DataRequired()])
+    skill_before = DecimalField('skill before:', places=1, validators=[DataRequired()])
+    skill_after = DecimalField('skill after: ', places=1, validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class DeleteReviewForm(FlaskForm):
+    review_id = IntegerField('Review ID', validators=[DataRequired()])
+    submit = SubmitField('Delete')
+
+
+class DeleteTopicForm(FlaskForm):
+    filename = SelectField('Topic:', choices=choices(), validators=[DataRequired()])
+    submit = SubmitField('Delete')
+
+class RenameTopicForm(FlaskForm):
+    old_filename = SelectField('Old name:', choices=choices(), validators=[DataRequired()])
+    new_filename = StringField('New name:', validators=[DataRequired()])
+    submit = SubmitField('Delete')
