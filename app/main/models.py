404c846b83441187374155892294b1997627db0a
jessicarush <hello@cyan.red>
2019-07-03 13:46:05 -0700

    Feature: count topics by loop number

--- app/main/models.py
+++ app/main/models.py
@@ -20,6 +20,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    # review_count = db.Column(db.Integer)
     # one to many relationship:
     repos = db.relationship('Repo', backref='user', lazy='dynamic')
 
